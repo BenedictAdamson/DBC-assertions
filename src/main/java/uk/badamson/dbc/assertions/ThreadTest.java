ba2308681a0795b0a5d834b09988acbb8b3ba12c
Benedict Adamson <badamson@spamcop.net>
2021-05-17 10:39:02 +0100

    Change internal copyright messages to EPL.

--- src/main/java/uk/badamson/dbc/assertions/ThreadTest.java
+++ src/main/java/uk/badamson/dbc/assertions/ThreadTest.java
@@ -1,21 +1,12 @@
 package uk.badamson.dbc.assertions;
 /*
- * © Copyright Benedict Adamson 2018,2021.
+ * Copyright 2018,2021 the original author or authors.
  *
- * This file is part of MC-des.
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
  *
- * MC-des is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * MC-des is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with MC-des.  If not, see <https://www.gnu.org/licenses/>.
+ * https://www.eclipse.org/legal/epl-v20.html
  */
 
 import java.util.ArrayList;
