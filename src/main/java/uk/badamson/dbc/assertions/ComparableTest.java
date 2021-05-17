0448816f7532f5a4cfe803a06ce42390278559bc
Benedict Adamson <badamson@spamcop.net>
2021-05-17 16:28:25 +0100

    Document ComparableTest.assertInvariants(T)

--- src/main/java/uk/badamson/dbc/assertions/ComparableTest.java
+++ src/main/java/uk/badamson/dbc/assertions/ComparableTest.java
@@ -12,6 +12,8 @@ package uk.badamson.dbc.assertions;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static uk.badamson.dbc.assertions.AssertAll.assertAll;
 
+import java.util.Objects;
+
 import javax.annotation.Nonnull;
 
 import org.opentest4j.AssertionFailedError;
@@ -20,8 +22,8 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 /**
  * <p>
- * Auxiliary test code for classes that implement the {@link Comparable}
- * interface.
+ * Verification methods to assist in unit testing classes that implement the
+ * {@link Comparable} interface.
  */
 @SuppressFBWarnings(justification = "Checking exceptions", value = "RV_RETURN_VALUE_IGNORED_NO_SIDE_EFFECT")
 public class ComparableTest {
@@ -48,6 +50,69 @@ public class ComparableTest {
         throw new AssertionFailedError("compareTo(null) throws NullPointerException", NullPointerException.class, null);
     }
 
+    /**
+     * <p>
+     * Assert that a given object conforms to all the invariants imposed by the
+     * {@link Comparable} interface, throwing an {@link AssertionError} if it does
+     * not.
+     * </p>
+     * 
+     * <h2>How to Use this Method</h2>
+     * <p>
+     * In your unit tests of mutators and constructors of classes that implement the
+     * {@link Comparable} class, you will check that the mutators and constructors
+     * make the desired changes, using test code similar to this:
+     * </p>
+     * 
+     * <pre>
+     * {@code @Test}
+     * public void increment_1() {
+     *    final var amount = new Amount(1);
+     *    
+     *    amount.increment();
+     *    
+     *    assertEquals(2, amount.intValue());
+     * }
+     * </pre>
+     *
+     * <p>
+     * But you can do do better than that. The class you are testing does not only
+     * have the behaviour that you have specified for it. It must also conform to
+     * some invariants imposed by the {@link Comparable} interface. You should also
+     * check that the mutated object (still) conforms to those invariants. There are
+     * a couple of them. Checking them can be fiddly. Explicitly checking them all
+     * directly in your test method would be verbose, error prone, and in some cases
+     * provide low value (because in that particular test, it is unlikely that the
+     * invariant would be broken).
+     * <p>
+     * </p>
+     * This method provides a convenient and abstract way to check that the mutated
+     * object still conforms to the invariants imposed by the {@link Comparable}
+     * interface. Simply delegate to this method in your test, like this:
+     * </p>
+     * 
+     * <pre>
+     * {@code @Test}
+     * public void increment_1() {
+     *    final var amount = new Amount(1);
+     *    
+     *    amount.increment();
+     *    
+     *    ObjectTest.assertInvariants(amount);
+     *    ComparableTest.assertInvariants(amount);
+     *    assertEquals(2, amount.intValue());
+     * }
+     * </pre>
+     * 
+     * @param <T>
+     *            The class of {@code object}
+     * @param object
+     *            The object to test.
+     * @throws NullPointerException
+     *             If {@code object} is null.
+     * @throws AssertionError
+     *             If {@code object} breaks an invariant.
+     */
     public static <T extends Comparable<T>> void assertInvariants(@Nonnull final T object) {
         assert object != null;
         assertAll(
@@ -70,10 +135,6 @@ public class ComparableTest {
 
     public static <T extends Comparable<T>> void assertInvariants(@Nonnull final T object1, @Nonnull final T object2,
             @Nonnull final T object3) {
-        assert object1 != null;
-        assert object2 != null;
-        assert object3 != null;
-
         final int c12 = compareTo(object1, object2);
         final int c23 = compareTo(object2, object3);
         final int c13 = compareTo(object1, object3);
@@ -88,6 +149,8 @@ public class ComparableTest {
     }
 
     private static <T extends Comparable<T>> int compareTo(@Nonnull final T object1, @Nonnull final T object2) {
+        Objects.requireNonNull(object1);
+        Objects.requireNonNull(object2);
         try {
             return object1.compareTo(object2);
         } catch (final Exception e) {
