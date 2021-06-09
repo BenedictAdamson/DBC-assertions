b9267cc051d9dbf093fb3d7d99854f2c2f6f4a2a
Benedict Adamson <badamson@spamcop.net>
2021-06-09 11:57:59 +0100

    Clarifier Verifier edge cases.

--- src/main/java/uk/badamson/dbc/assertions/Verifier.java
+++ src/main/java/uk/badamson/dbc/assertions/Verifier.java
@@ -11,6 +11,8 @@ package uk.badamson.dbc.assertions;
 
 import java.util.function.Consumer;
 
+import javax.annotation.Nullable;
+
 /**
  * A {@linkplain FunctionalInterface functional interface} representing a method
  * that <i>verifies</i> whether a value is correct, throwing an
@@ -21,5 +23,24 @@ import java.util.function.Consumer;
 @FunctionalInterface
 public interface Verifier<T> {
 
-    void verify(T t) throws AssertionError;
+    /**
+     * <p>
+     * Verify whether a given object reference is <dfn>correct</dfn>.
+     * </p>
+     * <p>
+     * In practice, if the method throws any kind of exception it is likely to be
+     * interpreted as a failure of the verification check, but an
+     * {@link AssertionError} should be the normal manner of doing so. If a
+     * verification check can fail by throwing any other kind of exception, the
+     * implementation should consider catching that exception and rethrowing a
+     * {@linkplain AssertionError#AssertionError(Object) chained AssertionError}
+     * exception.
+     * </p>
+     *
+     * @param t
+     *            The object reference to verify.
+     * @throws AssertionError
+     *             If, and only if, {@code t} fails the verification check.
+     */
+    void verify(@Nullable T t) throws AssertionError;
 }
